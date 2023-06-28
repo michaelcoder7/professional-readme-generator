c59044ce7a1b66e5aa99af14f81c4f1eca32fb28
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-06-28 11:48:23 +0100

    questions object in progress

--- index.js
+++ index.js
@@ -5,7 +5,34 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project's name?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project",
+  },
+  {
+    type: "list",
+    name: "liscense",
+    message: "What kind of liscense should your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
