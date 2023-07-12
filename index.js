e4893bc486dcd2a052da99bc7f37ab5c7d319f3f
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-07-12 12:28:38 +0100

    new file added and index file updated

--- index.js
+++ index.js
@@ -5,70 +5,21 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [
-  {
-    type: "input",
-    name: "github",
-    message: "What is your GitHub username?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is your email address?",
-  },
-  {
-    type: "input",
-    name: "title",
-    message: "What is your project's name?",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Please write a short description of your project",
-  },
-  {
-    type: "list",
-    name: "liscense",
-    message: "What kind of liscense should your project have?",
-    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "What command should be run to install dependencies?",
-    default: "npm i",
-  },
-  {
-    type: "input",
-    name: "test",
-    message: "What command should be run to run tests?",
-    default: "npm test",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "What does the user need to know about using the repo?",
-  },
-  {
-    type: "input",
-    name: "contributing",
-    message: "What does the user need to know about contributing to the repo?",
-  },
-];
+const questions = require("./utils/questions");
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-  return fs.writerFileSync(path.join(process.cwd(), filename), data);
+function writeToFile(markdown) {
+  fs.writerFile("readme.md", markdown, (err) =>
+    err ? console.log(err) : console.log("Successfully created index.html")
+  );
 }
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(questions).then((inquirerResponses) => {
-    console.log("Generating README...");
+  inquirer.prompt(questions).then((answers) => {
+    const markdown = generateMarkdown(answers);
 
-    const markdown = generateMarkdown({ ...inquirerResponses });
-
-    writeToFile("README.md", markdown);
+    writeToFile(markdown);
   });
 }
 
