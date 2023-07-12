5513491b1838477f12120d61b62f7e6ec48db83b
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-07-12 14:08:04 +0100

    markdown being generated plus index file edits

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
-const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const questions = require("./utils/questions");
